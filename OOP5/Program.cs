63a141273f506249500dca6a6173d02d9259c4eb
سلمى نجم الدين محمد حسن بيومى العوفى stu <425254647@sha.edu.eg>
2026-09-23 17:55:37 +0300

    Q 2

--- OOP5/Program.cs
+++ OOP5/Program.cs
@@ -13,7 +13,17 @@
             // No new object is created on the Heap.
             // Copying an Object: Allocates memory on the Heap and creates a new object containing duplicate values
             // from the original object.
-            // 
+            // Q 2
+            // a) A Shallow Copy creates a new object, but copies only the top-level fields.
+            // Primitive values are copied by value, while reference-type fields are copied as references 
+            //  b)A Deep Copy creates a new object and recursively creates new copies of all nested objects/reference types,
+            //  resulting in complete independence between the original and copied objects.
+            // c) Their references  are copied, meaning both the original
+            // and shallow-copied objects will share the exact same nested objects in memory.
+            // d) Entirely new instances of the reference-type members are instantiated in memory,
+            // copying their internal values so no references are shared.
+            // e)  When an object contains nested complex structures ,
+            // and you want to modify the cloned order's item details without affecting the original order's data.
         }
     }
 }
