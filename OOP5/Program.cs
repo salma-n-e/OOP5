6bb5a77ab84bcf3ee534e612511269deb9b266eb
سلمى نجم الدين محمد حسن بيومى العوفى stu <425254647@sha.edu.eg>
2026-09-23 17:49:31 +0300

    Q 1

--- OOP5/Program.cs
+++ OOP5/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            
+            // Q 1
+            // a) Only the reference is copied, not the actual object data.
+            //  Both variables now point to the exact same memory location on the Heap.
+            // b) No. It does not allocate new memory or create a new object. Any modification
+            // made through one variable will directly affect the other variable because they reference the same object.
+            // c) Copying a Reference: Copies only the pointer/address to the existing object.
+            // No new object is created on the Heap.
+            // Copying an Object: Allocates memory on the Heap and creates a new object containing duplicate values
+            // from the original object.
+            // 
         }
     }
 }
